57a5d574435fb2f48b611d6315ebdd75c876d619
vgalin5 <vgalin5@bancodebogota.com.co>
2025-06-04 22:04:54 -0500

    rutas back

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
-});
+  baseURL: '/api', // <- así está bien: una ruta relativa
+})
 
-export default api;
+export default api
