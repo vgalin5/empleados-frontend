35b1bf8e0ab5a63a82f9b6e2db0b48417ad8ebb3
vgalin5 <vgalin5@bancodebogota.com.co>
2025-06-04 20:30:06 -0500

    dependecias

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
-
+ 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
